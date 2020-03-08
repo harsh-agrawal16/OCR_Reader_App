1b0cf7c00c8a1e84ded8426bc1049555dd31cd73
harsh-agrawal16 <agrwl.harsh@gmail.com>
2020-03-08 11:58:42 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/ocrreaderapp/OcrCaptureActivity.java
@@ -0,0 +1,14 @@
+package com.example.ocrreaderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OcrCaptureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocr_capture);
+    }
+}
